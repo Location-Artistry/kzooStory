7a9165012bc009d5c6a6ea8c59c6f21ebe91485f
Location-Artistry <erickerney@gmail.com>
2021-04-29 21:29:10 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,287 @@
+var config = {
+    style: 'mapbox://styles/branigan/cjz37rcb003ib1cr3s8rnkt2d',
+    accessToken: 'pk.eyJ1IjoibWJ4c29sdXRpb25zIiwiYSI6ImNrMm01aG9hdTBlZGwzbXQ1ZXVrNHNmejAifQ.QHQA0N6XPWddCXtvoODHZg',
+    showMarkers: false,
+    theme: 'dark',
+    use3dTerrain: true,
+    title: 'Kalamazoo River Field Survey April 27th 2021',
+    subtitle: 'EGLE & Tribal float survey to review sediment deposits',
+    byline: '',
+    footer: 'Data Collection and Story by NHBP Environmental Staff <a href="https://nhbp-nsn.gov/environmental/">NHBP ENV</a> <a href="https://storymaps.arcgis.com/collections/2f9f61b09e5b4860b527eff83cc08fb4/">StoryMap Collection</a>',
+    chapters: [
+        {
+            id: 'kzooLanding',
+            alignment: 'center',
+            title: 'Morrow Pond November 2020',
+            image: 'https://drive.google.com/uc?export=download&id=1prwd_2ifZ1Da26CWsPPw2c4C9hJ2FlAe',
+            description: 'The Morrow Pond is a human made impoundment of the Kalamazoo River between Comstock and Galesburg, Michigan.  It was drawn down in late 2019 to repair a hydropower dam structure.  This UAV photo was snapped in November 2020.',
+            location: {
+                center: [-85.50457, 42.28606],
+                zoom: 11,
+                pitch: 0.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {
+                    layer: 'gnpglaciers-1998',
+                    opacity: 0.25
+                },
+                {
+                    layer: 'glaciernp-boundary',
+                    opacity: 0.25
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'glaciernp-boundary',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'merrill',
+            alignment: 'left',
+            title: 'Merrill Park, Comstock',
+            image: 'https://services5.arcgis.com/RsKCQCx9eUKixVOM/arcgis/rest/services/service_f13c39e417024074b738a4e6549c53aa/FeatureServer/0/2/attachments/2?token=ZbI4wkLviumrIl6Aj_XmeOkGqrRptUkdf2Hr5C7DkfsqFxc1L8gdeqig0-oiwbKavQOZaEBUlXSOknOYW4WfvXOKb-Kdoep48b8dIPqxvv6FBLpGCk7aKqqCCjnP-rpiB92jlgtPCIVtqGz7mZq4i8UrUB3F8ZY0wMvDb9I546D18XTBCNLmpo45qEdSXx49IMJTS0FMTkSJPVl0gwE99QbrTAV8OB13ZYFQS5Io6fhMZaRUQhYMqAHJZynYDi6pd2TPopALvoSf2G2uBn-DNXfURrDpJdAs_Lp0v_1DBT2vfAcG9LYvFh1fWIqtdZyj',
+            description: 'EGLE & Tribal staff prepare to launch at Merrill Park in Comstock on the Kalamazoo River.  The purpose of the survey was to inspect sediment released from the lowering of the impoundment.',
+            location: {
+                center: { lon: -85.53206, lat: 42.28058 },
+                zoom: 12.41,
+                pitch: 43.00,
+                bearing: -52.00
+            },
+            onChapterEnter: [],
+            onChapterExit: [
+                // {
+                //     layer: 'gnpglaciers-2015',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'harrison2015',
+            alignment: 'left',
+            title: 'Harrison Glacier, 2015',
+            image: '',
+            description: 'Between 1998 and 2015, Harrison Glacier lost 169 acres of surface area (about 37%).',
+            location: {
+                center: [-113.72917, 48.58938],
+                zoom: 12.92,
+                pitch: 39.50,
+                bearing: 36.00
+            },
+            onChapterEnter: [
+                {
+                    layer: 'gnpglaciers-2015',
+                    opacity: 0.25
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'gnpglaciers-2015',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'blackfoot1998',
+            alignment: 'left',
+            title: 'Blackfoot Glacier, 1998',
+            image: '',
+            description: 'Blackfoot Glacier is the second largest of the remaining 25 glaciers in Glacier National Park, Montana. Blackfoot Glacier is just to the north of Blackfoot Mountain and near Jackson Glacier. When first documented in 1850, the glacier also included the now separate Jackson Glacier and together, they covered 1,875 acres (7.59 km2). In 1850, there were an estimated 150 glaciers in the park. Glaciologists have stated that by the year 2030, all the glaciers in the park may disappear.',
+            location: {
+                center: [-113.66573, 48.59181],
+                zoom: 12.92,
+                pitch: 51.50,
+                bearing: -26.40
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'blackfoot2015',
+            alignment: 'left',
+            title: 'Blackfoot Glacier, 2015',
+            image: '',
+            description: 'Between 1998 and 2015, Blackfoot Glacier lost 31 acres of surface area (about 8%).',
+            location: {
+                center: [-113.66573, 48.59181],
+                zoom: 12.92,
+                pitch: 51.50,
+                bearing: -26.40
+            },
+            onChapterEnter: [
+                {
+                    layer: 'gnpglaciers-2015',
+                    opacity: 0.25
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'gnpglaciers-2015',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'agassiz1998',
+            alignment: 'left',
+            title: 'Agassiz Glacier, 1998',
+            image: '',
+            description: 'Agassiz Glacier is in Glacier National Park in the U.S. state of Montana. It is named after Louis Agassiz, a Swiss-American glaciologist. The glacier is situated in a cirque to the southeast of Kintla Peak west of the Continental Divide. Agassiz Glacier is one of several glaciers that have been selected for monitoring by the U.S. Geological Survey\'s Glacier Monitoring Research program, which is researching changes to the mass balance of glaciers in and surrounding Glacier National Park.',
+            location: {
+                center: [-114.15881, 48.93439],
+                zoom: 13.51,
+                pitch: 41.00,
+                bearing: 78.33
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'agassiz2015',
+            alignment: 'left',
+            title: 'Agassiz Glacier, 2015',
+            image: '',
+            description: 'Between 1998 and 2015, Agassiz Glacier lost 108 acres of surface area (about 37%).',
+            location: {
+                center: [-114.15881, 48.93439],
+                zoom: 13.51,
+                pitch: 41.00,
+                bearing: 78.33
+            },
+            onChapterEnter: [
+                {
+                    layer: 'gnpglaciers-2015',
+                    opacity: 0.25,
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'gnpglaciers-2015',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'rainbow1998',
+            alignment: 'left',
+            title: 'Rainbow Glacier, 1998',
+            image: '',
+            description: 'Rainbow Glacier is in Glacier National Park in the U.S. state of Montana. The glacier is situated immediately to the east of Rainbow Peak at an elevation between 8,500 feet (2,600 m) and 8,000 feet (2,400 m) above sea level. The glacier has visible crevasses in satellite imagery. Rainbow Glacier has shown modest retreat compared to other glaciers in Glacier National Park.',
+            location: {
+                center: [-114.08659, 48.88039],
+                zoom: 13.09,
+                pitch: 50.00,
+                bearing: -53.60
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'rainbow2015',
+            alignment: 'left',
+            title: 'Rainbow Glacier, 2015',
+            image: '',
+            description: 'Between 1998 and 2015, Rainbow Glacier lost 17 acres of surface area (about 6%).',
+            location: {
+                center: [-114.08659, 48.88039],
+                zoom: 13.09,
+                pitch: 50.00,
+                bearing: -53.60
+            },
+            onChapterEnter: [
+                {
+                    layer: 'gnpglaciers-2015',
+                    opacity: 0.25
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'gnpglaciers-2015',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'kintla1998',
+            alignment: 'left',
+            title: 'Kintla Glacier, 1998',
+            image: '',
+            description: 'Kintla Glacier is in Glacier National Park in the U.S. state of Montana. The glacier is situated on a plateau 2 miles (3.2 km) southwest of Kintla Peak at an elevation between 8,700 feet (2,700 m) and 7,700 feet (2,300 m) above sea level. The glacier has numerous crevasses and is actually two glaciers.',
+            location: {
+                center: [-114.18755, 48.92880],
+                zoom: 13.09,
+                pitch: 48.50,
+                bearing: 164.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'kintla2015',
+            alignment: 'left',
+            title: 'Kintla Glacier, 2015',
+            image: '',
+            description: 'Between 1998 and 2015, Harrison Glacier lost 24 acres of surface area (about 10%).',
+            location: {
+                center: [-114.18755, 48.92880],
+                zoom: 13.09,
+                pitch: 48.50,
+                bearing: 164.00
+            },
+            onChapterEnter: [
+                {
+                    layer: 'gnpglaciers-2015',
+                    opacity: 0.25
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'gnpglaciers-2015',
+                    opacity: 0
+                }
+            ]
+        },
+        {
+            id: 'sperry1998',
+            alignment: 'left',
+            title: 'Sperry Glacier, 1998',
+            image: '',
+            description: 'Sperry Glacier is a glacier on the north slopes of Gunsight Mountain west of the Continental Divide in Glacier National Park in the U.S. state of Montana. Although many geologic features of Glacier National Park were formed during the much longer period of glaciation ending over 10,000 years ago, Sperry Glacier — like all the glaciers in the park today — is a product of the recent Little Ice Age, the period of cooler average temperatures starting in about the 13th century and concluding in the mid-19th century.',
+            location: {
+                center: [-113.75672, 48.62433],
+                zoom: 13.68,
+                pitch: 34.50,
+                bearing: 106.40
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'sperry2015',
+            alignment: 'left',
+            title: 'Sperry Glacier, 2015',
+            image: '',
+            description: 'Between 1998 and 2015, Harrison Glacier lost 37 acres of surface area (about 16%).',
+            location: {
+                center: [-113.75672, 48.62433],
+                zoom: 13.68,
+                pitch: 34.50,
+                bearing: 106.40
+            },
+            onChapterEnter: [
+                {
+                    layer: 'gnpglaciers-2015',
+                    opacity: 0.25
+                }
+            ],
+            onChapterExit: [
+                {
+                    layer: 'gnpglaciers-2015',
+                    opacity: 0
+                }
+            ]
+         }
+    ]
+};
